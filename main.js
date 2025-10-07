1312f720c90abfbddd700eedcfa1a341826781b8
Phil <phillip20267541@gmail.com>
2025-10-07 09:28:51 +0100

    Update main.js
    - Removed unnecessary comma

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
     data() {
         content = [
             { title: 'CV', description: 'My CV', link: 'https://phil20267541.github.io/CV_Website/', image: 'Resources/cv.png' },
-            { title: 'Shoop', description: 'All hail the shoop', link: 'https://phil20267541.github.io/Shoop/', image: 'Resources/shoop.png' },
+            { title: 'Shoop', description: 'All hail the shoop', link: 'https://phil20267541.github.io/Shoop/', image: 'Resources/shoop.png' }
         ]
         projects = [{title:'CV', link: '#cv'}, {title:'Shoop', link: '#shoop'}]
       return {
