eed2efa3147bab986c9a010bd74b82e30d43ae1a
Phil <phillipswann2008@gmail.com>
2025-10-15 17:21:44 +0100

    Test

--- Backend/app.py
+++ Backend/app.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SUPABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
